a9fd0ea29e9cd2d17ab217b478f0bc563e8cf59a
Bob Hansen <github@grettir.org>
2022-01-12 12:16:55 -0800

    Cleaned up structured logging tests

--- tests/test_structured.cpp
+++ tests/test_structured.cpp
@@ -12,21 +12,23 @@ std::string log_info(const std::array<spdlog::Field, N>& fields, const T &what, 
 
     spdlog::logger oss_logger("oss", oss_sink);
     oss_logger.set_pattern("%v");
-    oss_logger.slog({}, spdlog::level::info, fields, what, args...);
+    oss_logger.slog({}, spdlog::level::info, fields, what, std::forward<ArgTypes>(args)...);
 
     return oss.str().substr(0, oss.str().length() - strlen(spdlog::details::os::default_eol));
 }
 
-TEST_CASE("fields ", "[field_construction]")
+TEST_CASE("fields ", "[structured]")
 {
+    // Can we construct fields of all types with lvalues?
     spdlog::F("var", 1);
     spdlog::F("var", "val");
 
+    // Can we construct fields with rvalues?
     std::string str1("str");
-    spdlog::F(str1, 1);
+    spdlog::F(str1, str1);
 }
 
-TEST_CASE("field_array ", "[field_array_construction]")
+TEST_CASE("build_fields ", "[structured]")
 {
     auto array0 = spdlog::build_fields();
     REQUIRE(array0.size() == 0);
@@ -44,13 +46,16 @@ TEST_CASE("field_array ", "[field_array_construction]")
     REQUIRE(to_string(std::get<spdlog::string_view_t>(array2[1].value)) == "two");
 }
 
-TEST_CASE("field_logging ", "[field_logging]")
+TEST_CASE("field_logging ", "[structured]")
 {
     // No fields
     REQUIRE(log_info(spdlog::NO_FIELDS, "Hello") == "Hello");
 
     // Some fields
     REQUIRE(log_info(spdlog::build_fields(spdlog::F("k", 1)), "Hello") == "Hello");
+
+    // Fields and fmt
+    REQUIRE(log_info(spdlog::build_fields(spdlog::F("k", 1)), "Hello {}", "world") == "Hello world");
 }
 
 TEST_CASE("buffered_msg_field_copies ", "[structured]")
